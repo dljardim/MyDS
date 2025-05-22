8fdab8f97e0b0155654401e40fcee6d566436b3e
dljardim <dljardim@outlook.com>
2025-05-22 12:32:35 -0500

    Initial Commit

---
+++ MyDS/MyDSApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyDSApp.swift
+//  MyDS
+//
+//  Created by Damian Jardim on 5/22/25.
+//
+
+import SwiftUI
+
+@main
+struct MyDSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
